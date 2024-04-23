c1c1d9f72e7774666f940fbf3bcad1b42ed01741
Moyon Enzo <moyon_e@etna-alternance.net>
2024-04-23 15:33:04 +0200

    switch shadcn theme slate -> red && some refactoring in App.svelte

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
     import VisibilityProvider from "./lib/components/layout/VisibilityProvider.svelte";
     import { debugData } from "$lib/utils/debugData";
+    import { visibility } from "$lib/utils/stores";
 
     import * as Card from "$lib/components/ui/card/index.js";
     import { Button } from "$lib/components/ui/button";
 
     import { ModeWatcher } from "mode-watcher";
+    import { fetchNui } from "$lib/utils/fetchNui";
 
-    debugData([
-        {
-            action: "setVisible",
-            data: true,
-        },
-    ]);
+    // Needed to set as ui visible in browser for debug
+    debugData([{ action: "setVisible", data: true }]);
+
+    function close() {
+        visibility.set(false);
+        fetchNui("hideUI");
+    }
 </script>
 
 <ModeWatcher />
@@ -21,13 +24,13 @@
     <Card.Root class="w-full max-w-sm">
         <Card.Header>
             <Card.Title class="text-2xl">Title</Card.Title>
-            <Card.Description
-                >Some description below this title</Card.Description
-            >
+            <Card.Description>
+                Some description below this title
+            </Card.Description>
         </Card.Header>
         <Card.Content class="grid gap-4"></Card.Content>
         <Card.Footer>
-            <Button class="w-full">Click me button</Button>
+            <Button class="w-full" on:click={close}>Close UI</Button>
         </Card.Footer>
     </Card.Root>
 </VisibilityProvider>
